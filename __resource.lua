a6ebf3bc5062a9dc0118b5db46fc1c229f6315fd
Elijah <145863832+Elijah-fivem@users.noreply.github.com>
2023-10-03 19:50:19 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Elijah'
+description 'Help Script Made By Elijah'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'help-c.lua',
+}
+
---
+++ help-c.lua
@@ -0,0 +1,10 @@
+RegisterCommand("help",function()
+    msg("change me in help-c.lua")
+    msg("change me in help-c.lua")
+    msg("change me in help-c.lua")
+    msg("change me in help-c.lua")
+end)
+
+function(msg) (text)
+    TriggerEvent ("chatMessage", "[Server]", {255,0,0}, text)
+end
